c61ad0bd4cd7da8c4a83cb4c8f6f0b086de120af
Hao Nguyen <graphicdesignhao@gmail.com>
2014-08-25 23:41:18 +0700

    + them trang login.php va profile.php

---
+++ phase2/login.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Game Kun - P2 - Login</title>
+    <link rel="stylesheet" type="text/css" href="css/p2_style.css" />
+    <link rel="stylesheet" type="text/css" href="css/p2_login.css" />
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+
+</head>
+
+<body>
+<h1>Sữa Kun</h1>
+<div id="topbar">
+    <div class="repeatmenu"></div>
+    <div class="wrapper_1000">
+        <div id="logo"><img src="images/logo-kunkun.png" title="logo_kunkun" alt="logo_kunkun" /></div>
+        <div class="iconKun"></div>
+        <div class="login"><a href="#">Đăng nhập</a><span></span></div>
+        <!-- ------ -->
+        <nav>
+            <ul>
+                <li><a href="#"><span></span>Giới thiệu</a></li>
+                <li><a href="#"><span></span>Thể lệ & Giải thưởng</a></li>
+                <li><a href="#"><span></span>Cuộc thi ảnh</a></li>
+                <li class="active"><a href="#"><span></span>Thư viện ảnh</a></li>
+                <li><a href="#"><span></span>Mẹo hay</a>
+                    <ul>
+                        <li><a href="#">Bí kíp của mẹ</a></li>
+                        <li><a href="#">Cẩm nang</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- ------ -->
+    </div>
+</div>
+
+<div class="wrapper_1000">
+    <div id="frame_login"></div>
+    <form class="flogin">
+        <h4>Bạn đã là thành viên?</h4>
+        <span>Email</span>
+        <input type="text" value="" />
+        <span>Mật khẩu</span>
+        <input type="text" value="" />
+        <input type="submit" value="Đăng nhập" />
+        <center>hoặc</center>
+        <input type="button" value="" />
+    </form>
+    <!-- -->
+    <form class="fregister">
+        <h4>Bạn chưa là thành viên của gia đình Kun? Hãy đăng ký ngay hôm nay:</h4>
+        <div class="fam"></div>
+        <div class="text"></div>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <div class="capt">MAE873A</div>
+        <input type="submit" value="Đăng ký" />
+    </form>
+</div>
+<div class="sunflower f7"></div>
+<div class="sunflower f1"></div>
+<div class="sunflower f2"></div>
+<div class="sunflower f3"></div>
+<div class="sunflower f4"></div>
+<div class="sunflower f5"></div>
+<div class="sunflower f6"></div>
+<div class="sunflower f8"></div>
+<div class="sunflower f9"></div>
+<div class="sunflower f10"></div>
+<div class="sunflower f11"></div>
+</body>
+</html>
